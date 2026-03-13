95fa5c6a942d8bf0743ffeb3286bb2746561e7f6
Xiao <caoxiao@live.com>
2026-03-13 16:10:57 +0100

    fix export variables

--- pipeline/slurm_submit.sh
+++ pipeline/slurm_submit.sh
@@ -29,21 +29,20 @@ export NUMBA_CACHE_DIR="/cfs/klemming/projects/supr/dmclab/numba_cachedir"
 export HF_HOME="/cfs/klemming/projects/supr/dmclab/hf_cachedir"
 export MPLCONFIGDIR="/cfs/klemming/projects/supr/dmclab/matplotlib_cachedir"
 export KACHERY_DIR="/cfs/klemming/projects/supr/dmclab/kachery_cachedir"
-export NXF_HOME="/cfs/klemming/projects/supr/dmclab/.nextflow-xiao"
 
 ############################
 # Pipeline paths
 ############################
 
+export DATA_PATH="/cfs/klemming/projects/supr/dmclab/xiao/SGL_DATA/vr1320251126_g0"
+
+export RESULTS_PATH="/cfs/klemming/projects/supr/dmclab/xiao/output/vr1320251126_g0"
+
+export PARAMS_FILE="$PIPELINE_PATH/pipeline/active_params.json"
+
 PIPELINE_PATH="/cfs/klemming/projects/supr/dmclab/aind-ephys-pipeline"
 
-DATA_PATH="/cfs/klemming/projects/supr/dmclab/xiao/SGL_DATA/vr1320251126_g0"
-
-RESULTS_PATH="/cfs/klemming/projects/supr/dmclab/xiao/output/vr1320251126_g0"
-
 WORKDIR="/cfs/klemming/projects/supr/dmclab/xiao/nextflow_work"
-
-PARAMS_FILE="$PIPELINE_PATH/pipeline/active_params.json"
 
 LOGDIR="/cfs/klemming/projects/supr/dmclab/nextflow_logs"
 
